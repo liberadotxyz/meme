33fdf2ffec967374d07ad425db6a8f0bbba8cb31
lobsang <lobsang1765@gmail.com>
2025-08-06 15:19:00 +0545

    new desing

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -2,29 +2,13 @@
 'use client';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
+import { Suspense } from 'react';
 
+import ErrorPageContent from './ErrorCompoent';
 export default function ErrorPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const error = searchParams.get('error');
-
-  useEffect(() => {
-    // If the error is due to cancellation, redirect to home.
-    // The specific error string might vary, but 'Callback' is a common one.
-    if (error === 'OAuthCallback' || error === 'Callback') {
-      router.push('/');
-    }
-  }, [error, router]);
-
-  // You can still display an error message for other, more serious errors
   return (
-    <div>
-      {error !== 'OAuthCallback' && error !== 'Callback' && (
-        <div style={{ color: 'red' }}>
-          An unexpected authentication error occurred. Please try again.
-        </div>
-      )}
-      <p>Redirecting to home...</p>
-    </div>
-  );
+     <Suspense fallback={<div>Loading...</div>}>
+      <ErrorPageContent />
+    </Suspense>
+  )
 }
--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,30 +1,12 @@
 // app/auth/error/page.tsx
 'use client';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
-import { TradeComponentSkeleton } from '@/components/skeleton/HmepageSkeleton';
+
+import SingInErrorPage from './SignInPage';
+import { Suspense } from 'react';
 export default function ErrorPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const error = searchParams.get('error');
-
-  useEffect(() => {
-    // If the error is due to cancellation, redirect to home.
-    // The specific error string might vary, but 'Callback' is a common one.
-    if (error === 'OAuthCallback' || error === 'Callback') {
-      router.push('/');
-    }
-  }, [error, router]);
-
-  // You can still display an error message for other, more serious errors
-  return (
-    <div>
-      {error !== 'OAuthCallback' && error !== 'Callback' && (
-        <div style={{ color: 'red' }}>
-          An unexpected authentication error occurred. Please try again.
-        </div>
-      )}
-      <TradeComponentSkeleton></TradeComponentSkeleton>
-    </div>
-  );
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+              <SingInErrorPage />
+            </Suspense>
+    )
 }
